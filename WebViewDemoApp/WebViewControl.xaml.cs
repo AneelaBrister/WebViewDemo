99734b1d0442655ff1975f358a8e9b706fe82495
Aneela Brister <aneela.brister@medsphere.com>
2021-08-06 15:11:26 -0700

    Add project files.

---
+++ WebViewDemoApp/WebViewControl.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WebViewDemoApp
+{
+    /// <summary>
+    /// Interaction logic for WebViewControl.xaml
+    /// </summary>
+    public partial class WebViewControl : UserControl
+    {
+        public WebViewControl()
+        {
+            InitializeComponent();
+        }
+
+        public void DisposeWebview()
+        {
+            this.webView.Dispose();
+        }
+    }
+}
---
+++ WebViewDemoApp/WebViewWindow.Designer.cs
@@ -0,0 +1,78 @@
+﻿using System.Diagnostics;
+using System.Windows;
+
+namespace WebViewDemoApp
+{
+    partial class WebViewWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            DisposeControls();
+            base.Dispose(disposing);
+        }
+
+        private void DisposeControls()
+        {
+            if (elementHost1 != null)
+            {
+                webViewControl1.DisposeWebview();
+                elementHost1.Dispose(); 
+                this.Controls.Remove(elementHost1);
+                elementHost1 = null;
+            }
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.elementHost1 = new System.Windows.Forms.Integration.ElementHost();
+            this.webViewControl1 = new WebViewDemoApp.WebViewControl();
+            this.SuspendLayout();
+            // 
+            // elementHost1
+            // 
+            this.elementHost1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.elementHost1.Location = new System.Drawing.Point(0, 0);
+            this.elementHost1.Name = "elementHost1";
+            this.elementHost1.Size = new System.Drawing.Size(1184, 661);
+            this.elementHost1.TabIndex = 0;
+            this.elementHost1.Text = "elementHost1";
+            this.elementHost1.Child = this.webViewControl1;
+            // 
+            // WebViewWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1184, 661);
+            this.Controls.Add(this.elementHost1);
+            this.Name = "WebViewWindow";
+            this.Text = "WebView Demo Window";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Integration.ElementHost elementHost1;
+        private WebViewControl webViewControl1;
+    }
+}
+
